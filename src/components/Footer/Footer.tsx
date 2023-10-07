b9218a1371c71dcf42ab31611648e74e3d0a0e72
Andrey rios <andreyrios.dev@gmail.com>
2023-10-07 07:33:09 -0500

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+// Libraries
+import { Link } from 'react-router-dom'
+// Styles
 import styles from './Footer.module.css'
+// Images
+import logo from '../../images/logo360.png'
+// Utils
+import { pathName } from '../../utils/pathName'
 
 function Footer() {
   return (
-    <p>Footer</p>
+    <footer className={styles.footer}>
+      <Link to={pathName.main} >
+        <figure className={styles.logo}>
+          <img src={logo} alt="HotelHub360" />
+        </figure>
+      </Link>
+    </footer>
   )
 }
 
